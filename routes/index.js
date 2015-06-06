b49f26c55f2cb93617106338f7ac6812f96268b8
SatanWoo <applewuziqi@gmail.com>
2015-06-06 17:16:38 +0800

    Ziqi: download all images url

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,35 @@ var router = express.Router();
 var renderer = require('../lib/renderer');
 
 router.get('/iframe', function(req, res) {
-  res.render('iframe');
+    res.render('iframe');
+});
+
+router.get('/download', function(req, res) {
+    var url = req.query.url;
+    console.log('url is ' + url);
+    var timeout = req.query.timeout || 3000;
+
+    if (!url) {
+        return res.json({error: 'no url specified'});
+    }
+
+    renderer.download(url, timeout, function(err, imgs) {
+        return res.json({html: imgs});
+    });
 });
 
 router.get('/render', function(req, res) {
-  var url = req.query.url;
-  console.log('url is ' + url);
-  var timeout = req.query.timeout || 3000;
-  if (!url) {
-    return res.json({error: 'no url specified'});
-  }
-  
-  renderer.render(url, timeout, function(err, html) {
-    return res.json({html: html});
-  });
+    var url = req.query.url;
+    console.log('url is ' + url);
+
+    var timeout = req.query.timeout || 3000;
+    if (!url) {
+       
+    }
+
+    renderer.render(url, timeout, function(err, html) {
+        return res.json({html: html});
+    });
 });
 
 module.exports = router;
