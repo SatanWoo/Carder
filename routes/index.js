58cab036a9552766a93859e4e2d50feb66bccfce
SatanWoo <applewuziqi@gmail.com>
2015-06-06 20:28:39 +0800

    Ziqi:fix for html

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,27 @@ var renderer = require('../lib/renderer');
 var lzx = require('../lib/lzx');
 
 router.get('/iframe', function(req, res) {
-  var url = req.query.url;
-  var source = req.query.source || 'default';
-  if (!url) return res.send('no url specified');
+    var url = req.query.url;
+    var source = req.query.source || 'default';
+    if (!url) return res.send('no url specified');
 
-  lzx.transform(url, source).
+    lzx.transform(url, source).
     then(function(data) {
-      res.render('iframe', {
-        query: req.query,
-        data: data
-      });
+        res.render('iframe', {
+            query: req.query,
+            data: data
+        });
     });
 });
 
 router.get('/render', function(req, res) {
-  var url = req.query.url;
-  var timeout = req.query.timeout || 3000;
-  if (!url) return res.json({error: 'no url specified'});
-  
-  renderer.render(url, timeout, function(err, html) {
-    return res.json({html: html});
-  });
+    var url = req.query.url;
+    var timeout = req.query.timeout || 3000;
+    if (!url) return res.json({error: 'no url specified'});
+
+    renderer.render(url, timeout, function(err, html) {
+        return res.json({html: html});
+    });
 });
 
 module.exports = router;
