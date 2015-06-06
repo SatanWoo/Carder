15c4811bf78469d38155f3439932420effe3ce53
John Wu <webmaster@leapoahead.com>
2015-06-06 19:48:58 +0800

    Ebay transformer working

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 ###### /render?url=[地址]&timeout=[等待JS渲染的时间(毫秒)]
 
+### iframe用法
+
+直接`<iframe src="http://xxx/iframe?url=xxxx" frameborder="0"></iframe>`是用算法计算
+
+`http://xxx/iframe?url=xxxx`可以带参数`source`，获取指定网站的卡片。例如，`http://xxx/iframe?url=xxxx&source=taobao`
+
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,27 @@
 var express = require('express');
 var router = express.Router();
 var renderer = require('../lib/renderer');
+var lzx = require('../lib/lzx');
 
 router.get('/iframe', function(req, res) {
-  res.render('iframe');
+  var url = req.query.url;
+  var source = req.query.source || 'default';
+  if (!url) return res.send('no url specified');
+
+  lzx.transform(url, source).
+    then(function(data) {
+      console.log('data: ' + JSON.stringify(data));
+      res.render('iframe', {
+        query: req.query,
+        data: data
+      });
+    });
 });
 
 router.get('/render', function(req, res) {
   var url = req.query.url;
-  console.log('url is ' + url);
   var timeout = req.query.timeout || 3000;
-  if (!url) {
-    return res.json({error: 'no url specified'});
-  }
+  if (!url) return res.json({error: 'no url specified'});
   
   renderer.render(url, timeout, function(err, html) {
     return res.json({html: html});
